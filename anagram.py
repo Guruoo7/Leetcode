908de1026a2a36e35556f6d3f535e4b654b38a9f
Guruprasanth <102672612+Guruoo7@users.noreply.github.com>
2023-03-14 22:46:39 +0530

    Add files via upload

--- anagram.py
+++ anagram.py
@@ -1,18 +1,11 @@
 def anagram(s, t):
-    sd = {}
-    td = {}
-    for i in s:
-        if i not in sd:
-            sd[i] = 1
-        else:
-            sd[i] += 1
-    for j in t:
-        if j not in td:
-            td[j] = 1
-        else:
-            td[j] += 1
-    if sd == td:
-        return True
-    return False
+    if len(s) != len(t):
+        return False
+    else:
+        for i in range(len(i)):
+            for j in range(i):
+                if s[i] == t[j]:
+                    return True
+        return False
 
 print(anagram("a", "ab"))
